b59b42b455b9bebfab87d9de9f323463c91f8e00
Wayne Thornton <TheFidoGroupColorado@outlook.com>
2020-03-16 17:47:09 -0600

    Added frameworks for Android, iOS and UWP applications.

---
+++ NOVID-20/NOVID-20/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using NOVID_20.Services;
+using NOVID_20.Views;
+
+namespace NOVID_20
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
